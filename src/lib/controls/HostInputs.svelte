8f7325e16d3cfda01bf4983b3f6a0d715dfe207d
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2022-10-31 20:55:42 +0100

    Fix stacking inputs issue

--- src/lib/controls/HostInputs.svelte
+++ src/lib/controls/HostInputs.svelte
@@ -7,13 +7,13 @@
   $: $hosts = Array(subnets).fill("");
 </script>
 
-<h2 class="text-gray-500 font-bold mt-10 mb-3">Number of hosts</h2>
+<h2 class="text-gray-500 font-bold mt-10 mb-0.5">Number of hosts</h2>
 
 <div class="flex flex-col">
   {#each $hosts as _, i}
     <input
       type="number"
-      class="border-2 border-gray-300 w-1/2 py-2 px-4 text-gray-700 leading-tight focus:outline-none {$errors[`hosts[${i}]`] ? 'border-red-500' : ''}"
+      class="border-2 border-gray-300 rounded my-1 w-1/2 py-2 px-4 text-gray-700 leading-tight focus:outline-none {$errors[`hosts[${i}]`] ? 'error' : ''}"
       placeholder="0"
       bind:value={$hosts[i]}
     />
--- src/lib/controls/Input.svelte
+++ src/lib/controls/Input.svelte
@@ -8,7 +8,7 @@
 
 <input
   type="text"
-  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:bg-white focus:outline-none focus:border-indigo-500 {error ? 'border-red-500 focus:border-red-500' : ''}"
+  class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:bg-white focus:outline-none focus:border-indigo-500 {error ? 'error' : ''}"
   {id}
   bind:value={$value}
 />
