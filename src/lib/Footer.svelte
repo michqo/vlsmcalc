a053c8ada8a0ee73b76aaf4c2e9d8bbded348abd
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-01-08 16:09:01 +0100

    Remove year and format

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="p-4 pb-8 bg-transparent flex items-center justify-center">
   <span class="text-xl text-slate-600 text-center"
-    >© 2022 <a href="https://github.com/MichalUSER" class="hover:underline"
+    >© <a href="https://github.com/MichalUSER" class="hover:underline"
       >MichalUser</a
     >
   </span>
